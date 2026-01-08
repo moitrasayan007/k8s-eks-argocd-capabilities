b4bf1d604718d471c07a020589d6879650e1c446
Sayan Moitra <moitrasayan007@gmail.com>
2026-01-08 21:02:44 +0530

    Add Helm chart structure

---
+++ push-helm-chart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variables
+AWS_REGION="us-west-1"
+AWS_ACCOUNT_ID="501294308535"
+CHART_NAME="nodejs-app"
+CHART_VERSION="0.1.0"
+ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+
+# Create ECR repository for Helm charts
+aws ecr create-repository --repository-name helm/${CHART_NAME} --region ${AWS_REGION} || true
+
+# Login to ECR
+aws ecr get-login-password --region ${AWS_REGION} | helm registry login --username AWS --password-stdin ${ECR_REGISTRY}
+
+# Package the chart
+helm package helm/${CHART_NAME}
+
+# Push to ECR
+helm push ${CHART_NAME}-${CHART_VERSION}.tgz oci://${ECR_REGISTRY}/helm
+
+echo "Chart pushed to: oci://${ECR_REGISTRY}/helm/${CHART_NAME}:${CHART_VERSION}"
+echo "Deploy with: kubectl apply -f argocd/nodejs-app-ecr.yaml"
